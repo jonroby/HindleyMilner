b13547654db93290eff35cae2acf444586b9be09
jonroby <35011179+jonroby@users.noreply.github.com>
2025-05-06 17:12:35 -0400

    type models

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,55 @@
-console.log("Hello Hindley Milner!")
+export type Expression =
+  | VariableExpression
+  | ApplicationExpression
+  | AbstractionExpression
+  | LetExpression;
+
+export interface VariableExpression {
+  type: "var";
+  x: string;
+}
+
+export interface ApplicationExpression {
+  type: "app";
+  x: string;
+}
+
+export interface AbstractionExpression {
+  type: "abs";
+  x: string;
+  e: Expression;
+}
+
+export interface LetExpression {
+  type: "let";
+  x: string;
+  e1: Expression;
+  e2: Expression;
+}
+
+export type MonoType =
+| TypeVariable
+| TypeFunctionApplication
+
+export type PolyType =
+| MonoType
+| TypeQuantifier
+
+export type TypeFunction = "->" | "Bool" | "Int" | "List"
+
+export interface TypeVariable {
+  type: 'ty-var',
+  a: string
+}
+
+export interface TypeFunctionApplication {
+  type: 'ty-app',
+  C: TypeFunction,
+  mus: MonoType[],
+}
+
+export interface TypeQuantifier {
+  type: 'ty-quantifier',
+  a: string,
+  sigma: PolyType,
+}
